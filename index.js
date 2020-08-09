8794fa0fd7f45e76d2263124a3169b7e853053e4
seoha <mooo4030@naver.com>
2020-08-09 16:01:39 +0900

    mongodb connect 보호

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
 const port = 3000;
+const app = express();
+const bodyParser = require('body-parser');
+const  { User } = require("./models/User");
+const config = require("./config/key");
 
+// application/x-www-form-urlencoded의 형식으로 오는 데이터는 받아서 파싱함
+app.use(bodyParser.urlencoded({extended:true}))
+
+// application/json 형식의 파일을 받음
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://seoha:1234@selena.z9owk.mongodb.net/selena?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('mongoDB connected..'))
 .catch(err=>console.log(err))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/',(req,res) => res.send('hello world 할룽베이베'))
+app.post('/register',(req,res) =>{
+    // 회원가입 시 필요한 정보를 데이터베이스에 저장
+    const user = new User(req.body);
+    user.save((err,doc) =>{
+        if(err) return res.json({success : false , err})
+        return res.status(200).json({
+            success : true
+        })
+    })
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port,() => {
+  console.log(`Example app listening on port ${port}!`);
 });
--- models/User.js
+++ models/User.js
@@ -32,3 +32,4 @@ const User = mongoose.model('User',userSchma)
 
 module.exports ={User}
 // 이 모델을 여러 파일에서도 사용할 수 있게 export
+// index.js에서 require하여 경로지정, cont { user }로 스키마를 감쌋던 user를 사용 할 수 있다.
